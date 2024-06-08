ec65d92072e46bbac540bcf63c3111bcc8031436
ZhongJess <sk2377328815@gmail.com>
2024-06-08 14:12:10 +0800

    改變menu和選單按鈕下拉
    改變menu和選單按鈕下拉

--- all.js
+++ all.js
@@ -1,16 +1,11 @@
-$(document).ready(function () {
-  // 透過 on 隨時監聽
-  $(".material-symbols-outlined").on("click", function (e) {
-    e.preventDefault();
-    $("body").toggleClass("menu-show");
-  });
+$(document).ready(function(){
 
-  $(".toggle-icon").on("click", function (event) {
-    event.preventDefault(); // 阻止<a>标签的默认行为
-    if ($(this).text() === "menu") {
-      $(this).text("close");
-    } else {
-      $(this).text("menu");
-    }
-  });
+  $('.navbar-btn').click(function(){
+
+      $('.navbar-btn').toggleClass('active');
+      $('.btn').toggleClass('close');
+      
+      $(".menu").toggleClass("active");
+  })
+
 });
